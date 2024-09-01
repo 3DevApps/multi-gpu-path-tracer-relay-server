57a0f5acd1909b75e0ecbddb6c449f9e561e0640
Patryk Klatka <patryk.klatka@gmail.com>
2024-09-01 16:58:24 +0200

    lol4

--- src/WebSocketConnectionStateHandler.js
+++ src/WebSocketConnectionStateHandler.js
@@ -10,6 +10,7 @@ class WebSocketConnectionStateHandler {
     const parsedUrl = new URL(req.url, "http://com.example");
 
     if (parsedUrl.searchParams.has("jobId")) {
+      const jobId = parsedUrl.searchParams.get("jobId");
       ws._jobId = jobId;
       if (parsedUrl.searchParams.has("path-tracing-job")) {
         this.clients.addPathTracingClient(
@@ -19,11 +20,12 @@ class WebSocketConnectionStateHandler {
         return;
       }
 
-      this.clients.addClientToJob(parsedUrl.searchParams.get("jobId"), ws);
+      this.clients.addClientToJob(jobId, ws);
       return;
     }
 
     const jobId = uuidv4();
+    ws._jobId = jobId;
     this.clients.addClientToJob(jobId, ws);
     ws.send("JOB_ID#" + jobId);
   }
